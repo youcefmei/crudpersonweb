4cd88e943bbf120ada7411b20de04476ce2fb845
youcefmei <youcefmei@aol.com>
2025-02-04 13:55:07 +0100

    Add: test

--- src/test/java/fr/afpa/pompey/cda/appweb/AppWebApplicationTests.java
+++ src/test/java/fr/afpa/pompey/cda/appweb/AppWebApplicationTests.java
@@ -1,13 +1,36 @@
 package fr.afpa.pompey.cda.appweb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AppWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void gestPersonTest() throws Exception {
+
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(MockMvcResultMatchers.content().string(containsString("Pennington")));
+    }
 }
